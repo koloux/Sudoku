878af3acb4cac2ce5d858193141aea2fcba0395d
Nicolas HUBER <nhuber@e2r1p7.42.fr>
2015-10-25 09:36:22 +0100

    yo

--- ft_set_case.c
+++ ft_set_case.c
@@ -6,23 +6,22 @@
 /*   By: jplevy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/24 14:56:23 by jplevy            #+#    #+#             */
-/*   Updated: 2015/10/24 16:13:07 by jplevy           ###   ########.fr       */
+/*   Updated: 2015/10/25 09:34:46 by nhuber           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int		ft_check_line(int nb, int pos,char tab[9][9])
+int		ft_check_line(int nb, int pos, char tab[9][9])
 {
-	int		line = pos / 9;
+	int		line;
 	int		i;
 
+	line = pos / 9;
 	i = 0;
 	while (i < 9)
 	{
-		if(i != pos % 9)
+		if (i != pos % 9)
 		{
-			if(tab[line][i] == '0' + nb)
+			if (tab[line][i] == '0' + nb)
 			{
 				return (1);
 			}
@@ -32,17 +31,18 @@ int		ft_check_line(int nb, int pos,char tab[9][9])
 	return (0);
 }
 
-int		ft_check_col(int nb, int pos,char tab[9][9])
+int		ft_check_col(int nb, int pos, char tab[9][9])
 {
-	int		col = pos % 9;
+	int		col;
 	int		i;
 
+	col = pos % 9;
 	i = 0;
 	while (i < 9)
 	{
-		if(i != pos / 9)
+		if (i != pos / 9)
 		{
-			if(tab[i][col] == '0' + nb)
+			if (tab[i][col] == '0' + nb)
 			{
 				return (1);
 			}
@@ -52,22 +52,24 @@ int		ft_check_col(int nb, int pos,char tab[9][9])
 	return (0);
 }
 
-int		ft_check_block(int nb, int pos,char tab[9][9])
+int		ft_check_block(int nb, int pos, char tab[9][9])
 {
-	int		col = pos % 9;
-	int		line = pos / 9;
+	int		col;
+	int		line;
 	int		i;
 	int		j;
 
+	col = pos % 9;
+	line = pos / 9;
 	i = (col / 3) * 3;
 	while (i < ((col / 3) * 3) + 3)
 	{
 		j = (line / 3) * 3;
 		while (j < ((line / 3) * 3) + 3)
 		{
-			if(i != pos % 9 || j != pos / 9)
+			if (i != pos % 9 || j != pos / 9)
 			{
-				if(tab[j][i] == '0' + nb)
+				if (tab[j][i] == '0' + nb)
 					return (1);
 			}
 			j++;
@@ -77,10 +79,17 @@ int		ft_check_block(int nb, int pos,char tab[9][9])
 	return (0);
 }
 
-int		main()
+int		ft_new_number(int n, int pos, char tab[9][9])
 {
-	char	tab[9][9] = {"9...7....", "2...9..53", ".6..124..", "84...1.9.", "5.....8..", ".31..4...", "..37..68.", ".9..5.741", "47......."};
-
-	printf("%d", ft_check_block(4, 40, tab));
+	while (n++ <= 9)
+	{
+		if (ft_check_line(n, pos, tab) == 0 &&
+				ft_check_col(n, pos, tab) == 0 &&
+				ft_check_block(n, pos, tab) == 0)
+		{
+			tab[pos / 9][pos % 9] = n;
+			return (1);
+		}
+	}
 	return (0);
 }
---
+++ main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhuber <nhuber@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/25 09:20:35 by nhuber            #+#    #+#             */
+/*   Updated: 2015/10/25 09:36:01 by nhuber           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+int		ft_new_number(int n, int pos, char tab[9][9]);
+void	ft_print(char **grid);
+
+int		main(int argc, char **argv)
+{
+	if (argc != 10 || ft_is_argv(argv) != 1)
+		write(1, "Erreur", 6);
+		return (0);
+
+	ft_print(argv);
+	return (0);
+}
